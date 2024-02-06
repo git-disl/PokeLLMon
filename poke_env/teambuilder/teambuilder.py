f3f34f6064463678dab13ea5b4f7aefed11c53b4
Ikko Eltociear Ashimine <eltociear@gmail.com>
2024-02-07 02:39:42 +0900

    Update teambuilder.py
    decribed -> described

--- poke_env/teambuilder/teambuilder.py
+++ poke_env/teambuilder/teambuilder.py
@@ -14,7 +14,7 @@ class Teambuilder(ABC):
     They must implement the yield_team method, which must return a valid
     packed-formatted showdown team every time it is called.
 
-    This format is a custom format decribed in Pokemon's showdown protocol
+    This format is a custom format described in Pokemon's showdown protocol
     documentation:
     https://github.com/smogon/pokemon-showdown/blob/master/PROTOCOL.md#team-format
 
